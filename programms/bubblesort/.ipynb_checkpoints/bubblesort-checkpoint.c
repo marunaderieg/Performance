cadc82a9d71dc224e1b505a8087e6b53bf6e8b83
Awesome Student <maruna.derieg@studium.fernuni-hagen.de>
2021-01-12 06:14:35 +0000

    first commit

---
+++ programms/bubblesort/.ipynb_checkpoints/bubblesort-checkpoint.c
@@ -0,0 +1,81 @@
+/* Copyright for this code belongs to https://github.com/Malhadas/Parallel-BubbleSort-/blob/master/bubble.c. Code has been edited by Maruna Derieg. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void bubble_sort(int a[], int n);
+void validate_sort (int n, int *data);
+void swap();
+void bubblesort_parallel (int a[], int N);
+
+int main(int argc, char* argv[]) {
+    //measure time    
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+    
+    //initialize data
+    int n = 1000000;
+    int i,j;
+    if (argc > 1) {
+            n = atoi(argv[1]);
+    }
+    else {
+        printf ("No value for list length (first argument) has been provided. Default length is 1000000.\n");
+    }
+    
+    // Generate the array.
+    int *a = (int *)malloc (sizeof (int) * n);
+    for (i=0; i<n; i++ ) {
+        a[i] = rand()%n;
+    }
+    
+    //quicksort algorithm
+    bubblesort_parallel(a, n);
+    
+    //measure time
+    clock_gettime(CLOCK_REALTIME, &end); 
+    double time_spent_sec= (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec)/1e9;
+    printf("Time spent: %g \n",time_spent_sec);
+    
+    //check if result is correct and free up space
+    validate_sort (n, &a[0]);
+    free (a);
+} 
+
+
+void bubblesort_parallel (int a[], int N) {
+    int i=0, j=0; 
+    int first;
+    for( i = 0; i < N-1; i++ )
+    {
+        first = i % 2; 
+        #pragma omp parallel for default(none),shared(a,first,N)
+        for( j = first; j < N-1; j += 1 )
+        {
+            if(a[j]>a[j+1])
+            {
+                swap(&a[j], &a[j+1]);
+            }
+        }
+    }
+}
+
+
+void swap(int *num1, int *num2)
+{
+
+    int temp = *num1;
+    *num1 =  *num2;
+    *num2 = temp;
+}
+
+void validate_sort (int n, int *a){
+    int i;
+    for (i = 0; i < n - 1; i++) {
+        if (a[i] > a[i+1]) {
+            printf ("ERROR: Validate failed\n");
+            break;
+        }
+    }
+}
---
+++ programms/mergesort/mergesort.c
@@ -0,0 +1,129 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <omp.h>
+#include <time.h>
+#pragma cling load("libomp.so")
+
+ /* Copyright belongs to https://github.com/avcourt, code accessed on https://github.com/avcourt/task-parallelism-omp/blob/master/code/merge_tasks_unnested.c. Code edited by Maruna Derieg. */
+
+
+void merge(int a[], int size, int temp[]);
+
+void mergesort_serial(int a[], int size, int temp[]);
+
+void mergesort_parallel_omp(int a[], int size, int temp[], int thresh);
+
+
+int main(int argc, char *argv[]) {
+    
+    //measure time    
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+    
+    if (argc != 2)
+    {
+        printf("Usage: %s array_size threshold\n", argv[0]);
+        return 1;
+    }
+
+    int size = atoi(argv[1]);       // Array size
+    int thresh = 100;    // Threshold
+
+    // Array allocation
+    int *a = malloc(sizeof(int) * size);
+    int *temp = malloc(sizeof(int) * size);
+    if (a == NULL || temp == NULL) {
+        printf("Error: Could not allocate array of size %d\n", size);
+        return 1;
+    }
+
+    // array initialization
+    for (int i = 0; i < size; i++) {
+        a[i] = rand() % size;
+    }
+
+    // run sort and get time
+    #pragma omp parallel
+    {
+        #pragma omp single nowait
+            mergesort_parallel_omp (a, size, temp, thresh);
+    }   
+ 
+    //measure time
+    clock_gettime(CLOCK_REALTIME, &end); 
+    double time_spent_sec= (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec)/1e9;
+    printf("Time spent: %g \n",time_spent_sec);
+
+    //validate if sorted correctly
+    for (int i = 1; i < size; i++) {
+        if (!(a[i - 1] <= a[i])) {
+            printf("Error: final array not sorted => a[%d]=%d > a[%d]=%d\n", i - 1,
+                   a[i - 1], i, a[i]);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// OpenMP merge sort with given number of threads
+void mergesort_parallel_omp(int a[], int size, int temp[], int thresh) {
+    if (size <= thresh) {
+        mergesort_serial(a, size, temp);
+    }
+    else {
+        #pragma omp task
+        {
+            mergesort_parallel_omp(a, size / 2, temp, thresh);
+        }
+        #pragma omp task
+        {
+            mergesort_parallel_omp(a + size / 2, size - size / 2,
+                                   temp + size / 2, thresh);
+        }
+        #pragma omp taskwait
+        {
+            merge(a, size, temp);
+        }
+
+    }
+}
+
+
+// only called if num_threads = 1
+void mergesort_serial(int a[], int size, int temp[]) {
+    // Switch to insertion sort for small arrays
+    if (size > 1) {
+        mergesort_serial(a, size / 2, temp);
+        mergesort_serial(a + size / 2, size - size / 2, temp);
+        merge(a, size, temp);
+    }
+}
+
+void merge(int a[], int size, int temp[]) {
+    int i1 = 0;
+    int i2 = size / 2;
+    int tempi = 0;
+    while (i1 < size / 2 && i2 < size) {
+        if (a[i1] < a[i2]) {
+            temp[tempi] = a[i1];
+            i1++;
+        } else {
+            temp[tempi] = a[i2];
+            i2++;
+        }
+        tempi++;
+    }
+    while (i1 < size / 2) {
+        temp[tempi] = a[i1];
+        i1++;
+        tempi++;
+    }
+    while (i2 < size) {
+        temp[tempi] = a[i2];
+        i2++;
+        tempi++;
+    }
+    // Copy sorted temp array into main array, a
+    memcpy(a, temp, size * sizeof(int));
+}
